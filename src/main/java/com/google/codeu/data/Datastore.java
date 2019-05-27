697860178badda9037ada8c1ada3930f29370705
t-weathers <38232272+t-weathers@users.noreply.github.com>
2019-05-27 15:04:11 -0700

    added Datastore logic to generate list of users who have posted a message

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -26,6 +26,8 @@ import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
+import java.util.Set;
+import java.util.HashSet;
 
 /** Provides access to the data stored in Datastore. */
 public class Datastore {
@@ -78,5 +80,21 @@ public class Datastore {
     }
 
     return messages;
+  }
+
+  /**
+   *Gets list of all users
+   *
+   *@return a Set of users, no order to this list, empty list if there are no users that have posted
+   */
+
+  public Set<String> getUsers(){
+    Set<String> users = new HashSet<>();
+    Query query = new Query("Message");
+    PreparedQuery results = datastore.prepare(query);
+    for(Entity entity : results.asIterable()) {
+      users.add((String) entity.getProperty("user"));
+    }
+    return users;
   }
 }

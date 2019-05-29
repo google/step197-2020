9009234c57233e55c5554ad66a400797115136d9
Thomas Weathers <thomasweathers415@outlook.com>
2019-05-29 11:21:52 -0700

    Update Datastore.java
    updated style features of Datastore.java

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -83,16 +83,15 @@ public class Datastore {
   }
 
   /**
-   *Gets list of all users
+   * Gather a list of all users that have entered something into the community chat. 
    *
-   *@return a Set of users, no order to this list, empty list if there are no users that have posted
+   * @return a Set of users, no order to this list, empty list if there are no users that have posted
    */
-
   public Set<String> getUsers(){
     Set<String> users = new HashSet<>();
     Query query = new Query("Message");
     PreparedQuery results = datastore.prepare(query);
-    for(Entity entity : results.asIterable()) {
+    for (Entity entity : results.asIterable()) {
       users.add((String) entity.getProperty("user"));
     }
     return users;

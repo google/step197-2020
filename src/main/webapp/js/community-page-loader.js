b37802a3221ec74bbdb6dd08fe64eca0faa9d8a4
t-weathers <38232272+t-weathers@users.noreply.github.com>
2019-06-08 16:21:46 -0700

    convert Community Page loader to React

--- src/main/webapp/js/community-page-loader.js
+++ src/main/webapp/js/community-page-loader.js
@@ -1,33 +1,81 @@
+const React = window.React;
+const ReactDOM = window.ReactDOM;
+const h = React.createElement;
+const root = document.getElementById("list");
 
-/** Fetches users and adds them to the page. */
-function fetchUserList(){
-const url = '/user-list';
-fetch(url).then((response) => {
-  return response.json();
-}).then((users) => {
-  const list = document.getElementById('list');
-  list.innerHTML = '';
+class Community extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.fetchUserList = this.fetchUserList.bind(this);
+  }
 
-  users.forEach((user) => {
-    const userListItem = buildUserListItem(user);
-    list.appendChild(userListItem);
-    });
-  });
+  render() {
+    const { Users } = this.state;
+    if (Users == null) {
+      return h("li", null, "loading...");
+    }
+    const children = [];
+    for (const user of Users) {
+      children.push(h(CommunityUser, { email: user }));
+    }
+    return h("li", null, children);
+  }
+
+  fetchUserList() {
+    const url = "/user-list";
+    fetch(url)
+      .then(response => {
+        return response.json();
+      })
+      .then(users => {
+        this.setState({ Users: users });
+        console.log(users);
+      });
+  }
+
+  componentDidMount() {
+    this.fetchUserList();
+    this.timer = window.setInterval(this.fetchUserList, 500);
+  }
+  componentWillUnmount() {
+    window.clearInterval(this.timer);
+  }
 }
-/**
-* Builds a list element that contains a link to a user page, e.g.
-* <li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>
+
+class CommunityUser extends React.Component {
+  render() {
+    const email = this.props.email;
+    return h(
+      "li",
+      null,
+      h("a", { href: "/user-page.html?user=" + email }, email)
+    );
+  }
+}
+
+ReactDOM.render(h(Community), root);
+
+/*
+<li>
+  <a href={"/user-page.html?user="+ email}> {name} {email}
+  </a>
+</li>
 */
-function buildUserListItem(user){
-  const userLink = document.createElement('a');
-  userLink.setAttribute('href', '/user-page.html?user=' + user);
+/** Fetches users and adds them to the page. */
+
+/*
+ * Builds a list element that contains a link to a user page, e.g.
+ * <li><a href="/user-page.html?user=test@example.com">test@example.com</a></li>
+ */
+/*function buildUserListItem(user) {
+  const userLink = document.createElement("a");
+  userLink.setAttribute("href", "/user-page.html?user=" + user);
   userLink.appendChild(document.createTextNode(user));
-  const userListItem = document.createElement('li');
+  const userListItem = document.createElement("li");
   userListItem.appendChild(userLink);
   return userListItem;
 }
 
+*/
 /** Fetches data and populates the UI of the page. */
-function buildUI(){
-  fetchUserList();
-}

57e631761eafc4cadf834e7ece275a5d7eb7c3cd
t-weathers <38232272+t-weathers@users.noreply.github.com>
2019-06-15 10:53:30 -0700

    Add 'create new pins' functionality

--- src/main/webapp/js/index.js
+++ src/main/webapp/js/index.js
@@ -1,9 +1,45 @@
+src =
+  "https://maps.googleapis.com/maps/api/js?key=AIzaSyB8qFRVjUKlhJERpMDx9RIzjqiOsl4tIMM";
 
-src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB8qFRVjUKlhJERpMDx9RIzjqiOsl4tIMM"
+function createMap() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.422, lng: -122.084 },
+    zoom: 16
+  });
+  addLandmark(
+    map,
+    37.423829,
+    -122.092154,
+    "Google West Campus",
+    "Google West Campus is home to YouTube and Maps."
+  );
+  addLandmark(
+    map,
+    37.421903,
+    -122.084674,
+    "Stan the T-Rex",
+    "This is Stan, the T-Rex statue."
+  );
+  addLandmark(
+    map,
+    37.420919,
+    -122.086619,
+    "Permanente Creek Trail",
+    "Permanente Creek Trail connects Google to a system of bike trails."
+  );
+}
 
-function createMap(){
-    const map = new google.maps.Map(document.getElementById('map'), {
-      center: {lat: 37.422, lng: -122.084},
-      zoom: 16
-    });
-  }
+/** Adds a marker that shows an info window when clicked. */
+function addLandmark(map, lat, lng, title, description) {
+  const marker = new google.maps.Marker({
+    position: { lat: lat, lng: lng },
+    map: map,
+    title: title
+  });
+  const infoWindow = new google.maps.InfoWindow({
+    content: description
+  });
+  marker.addListener("click", function() {
+    infoWindow.open(map, marker);
+  });
+}

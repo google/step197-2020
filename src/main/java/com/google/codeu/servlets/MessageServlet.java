1216b616dfbb8000d9c1c2dfb0dc5e585074b9ca
Tyler Hou <tylerhou@google.com>
2019-06-26 20:16:04 -0700

    Revert "Revert "Have 404's redirect to root. (#25)" (#27)" (#28)
    This reverts commit 57e45a65ae455b34ebbed6f7b012ae2b29d15f96.

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -74,7 +74,7 @@ public class MessageServlet extends HttpServlet {
 
     UserService userService = UserServiceFactory.getUserService();
     if (!userService.isUserLoggedIn()) {
-      response.sendRedirect("/index.html");
+      response.sendRedirect("/");
       return;
     }
 

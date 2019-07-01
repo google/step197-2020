506970c9bce6c10c1408e4df08982512bfdb50ff
Sophia <sophia.qin@berkeley.edu>
2019-06-30 23:14:00 -0700

    add new servlet addPlace

---
+++ frontend/src/addPlace.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class AddPlace extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: "", x_coord: "", y_coord: "" };
+  }
+
+  render() {
+    const { name, x_coord, y_coord } = this.state;
+    const placeObject = { name, x_coord, y_coord };
+
+    return (
+      <div>
+        <h1> Add Place </h1>
+        <textarea>Enter a location name</textarea>
+        <br />
+        <textarea>Enter a X coord</textarea>
+        <br />
+        <textarea>Enter a Y coord</textarea>
+        <br />
+        <button
+          onClick={() =>
+            fetch("/api/place", {
+              method: "POST",
+              credentials: "same-origin",
+              headers: { "Content-Type": "application/json" },
+              body: JSON.stringify(placeObject)
+            }).then(() => window.location.reload())
+          }
+        >
+          Submit
+        </button>
+      </div>
+    );
+  }
+}
+
+// Fetch data and populate the UI of the page.
+const root = document.getElementById("root");
+ReactDOM.render(<AddPlace />, root);

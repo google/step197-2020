828c6a6993a1b8aca65f0610460ec45e560e5073
Sophia <sophia.qin@berkeley.edu>
2019-07-16 23:16:20 -0700

    created ability to upload photos while adding a place.

--- frontend/src/addPlace.js
+++ frontend/src/addPlace.js
@@ -4,32 +4,62 @@ import ReactDOM from "react-dom";
 class AddPlace extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { name: "", x_coord: "", y_coord: "" };
+    this.state = { name: "", x_coord: "", y_coord: "", image: "" };
   }
 
   render() {
-    const { name, x_coord, y_coord } = this.state;
-    const placeObject = { name, x_coord, y_coord };
+    const { name, x_coord, y_coord, image } = this.state;
+    const placeObject = { name, x_coord, y_coord, image };
 
     return (
       <div>
         <h1>Add Place</h1>
-        <textarea value={this.state.value} onChange={e => {this.setState({ name: e.target.value})}}>
-        Enter a location name</textarea>
+        <textarea
+          placeholder={"Enter a name"}
+          value={this.state.name}
+          onChange={e => {
+            this.setState({ name: e.target.value });
+          }}
+        ></textarea>
         <br />
-        <textarea value={this.state.value} onChange={e => {this.setState({ x_coord: e.target.value })}}>Enter a X coord</textarea>
+        <textarea
+          placeholder={"Enter a X-coord"}
+          value={this.state.x_coord}
+          onChange={e => {
+            this.setState({ x_coord: e.target.value });
+          }}
+        ></textarea>
         <br />
-        <textarea value={this.state.value} onChange={e => {this.setState({ y_coord: e.target.value })}}>Enter a Y coord</textarea>
+        <textarea
+          placeholder={"Enter a Y-coord"}
+          value={this.state.y_coord}
+          onChange={e => {
+            this.setState({ y_coord: e.target.value });
+          }}
+        ></textarea>
+        <br />
+        Select image to upload:
+        <input
+          type="file"
+          name="fileToUpload"
+          onChange={e => {
+            this.setState({ image: e.target.files[0] });
+          }}
+        />
         <br />
         <button
-          onClick={() =>
+          onClick={() => {
+            const formData = new FormData();
+            formData.append("name", this.state.name);
+            formData.append("x_coord", this.state.x_coord);
+            formData.append("y_coord", this.state.y_coord);
+            formData.append("image", this.state.image);
             fetch("/api/place", {
               method: "POST",
               credentials: "same-origin",
-              headers: { "Content-Type": "application/json" },
-              body: JSON.stringify(placeObject)
-            }).then(() => window.location.reload())
-          }
+              body: formData
+            }).then(() => window.location.reload());
+          }}
         >
           Submit
         </button>

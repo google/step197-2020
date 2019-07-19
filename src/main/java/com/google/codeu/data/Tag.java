211dfe73dd13d8af91976941422f2fb3601a2145
Janet Vu <vjanet@umich.edu>
2019-07-19 16:22:53 -0700

    Recreated #46 to avoid rebasing merge conflicts (#49)
    * Recreated #46 to avoid rebasing merge conflicts
    
    Implemented suggestions from https://github.com/jajanet/team-08-codeu/pull/46 to create tag class
    
    * Fix syntax
    
    Also, unsure if this would need a UUID? If something has the same label, I think it's appropriate to say it's the same label even if attached to different places or users

---
+++ src/main/java/com/google/codeu/data/Tag.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2019 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.codeu.data;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.Entity;
+
+/** A tag, which can map to many different places. */
+public class Tag {
+
+  private String label;
+
+  /**
+   * Constructs a new {@link Tag} posted using only a label
+   */
+  public Tag(String label) {
+    this.label;
+  }
+  
+  /** Return Tag data using based on entity from search query. */
+  public Tag(Entity entity) {
+    this.label = (String)entity.getProperty("label");
+  }
+
+  public String getLabel() {
+    return label;
+  }
+
+}

1bf0c97ac6ecf6dacbfabb86f24de339d09223a7
Janet Vu <vjanet@umich.edu>
2019-07-19 22:32:47 -0700

    Fix tag error - need get/store methods (#52)
    * Fix tag error
    
    * Create get methods for tag
    
    * Compile tags successfully
    
    * Add id to tags
    
    * Remove store and fix casting of UUID
    
    * Remove redundant string casting

--- src/main/java/com/google/codeu/data/Tag.java
+++ src/main/java/com/google/codeu/data/Tag.java
@@ -18,32 +18,76 @@
 package com.google.codeu.data;
 import com.google.appengine.api.datastore.Key;
 import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.PreparedQuery;
+import com.google.appengine.api.datastore.Query;
+import com.google.appengine.api.datastore.Query.Filter;
+import com.google.appengine.api.datastore.Query.FilterOperator;
+import com.google.appengine.api.datastore.Query.FilterPredicate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 
 
 
 /** A tag, which can map to many different places. */
-/*
 public class Tag {
 
+  private UUID id;
   private String label;
+  
+  /**
+   * Constructs a new {@link Tag} with a label and a random ID
+   */
+  public Tag(String label) {
+    this(UUID.randomUUID(), label);
+  }
 
   /**
    * Constructs a new {@link Tag} posted using only a label
    */
-
-/*
-  public Tag(String label) {
-    this.label;
+  public Tag(UUID id, String label) {
+    this.id = id;
+    this.label = label;
   }
-   */
+
   /** Return Tag data using based on entity from search query. */
- /* public Tag(Entity entity) {
+  public Tag(Entity entity) {
     this.label = (String)entity.getProperty("label");
+    this.id = UUID.fromString(entity.getProperty("id"));
+  }  
+  
+  public static List<Tag> getAll() {
+    DatastoreService datastore = Datastore.GetSingletonService();
+    Query query = new Query("Tag");
+    PreparedQuery results = datastore.prepare(query);
+    List<Tag> tags = new ArrayList<Tag>();
+    for (Entity entity : results.asIterable()) {
+      tags.add(new Tag(entity));
+    }
+    return tags;
+  }
+
+  
+  public static Tag getByLabel(String label) {
+    DatastoreService datastore = Datastore.GetSingletonService();
+    Filter tagFilter = new FilterPredicate("label", FilterOperator.EQUAL, label);
+    Query query = new Query("Tag").setFilter(tagFilter);
+    Entity entity = datastore.prepare(query).asSingleEntity();
+    
+    if (entity == null) {
+      return null;
+    }
+    return new Tag(entity);
   }
 
   public String getLabel() {
     return label;
   }
+  
+  public UUID getId() {
+    return id;
+  }
 
-} */
+}

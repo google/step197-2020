7848d24b650bc9984a0ac2a4b9135c0ec4781443
Janet Vu <vjanet@umich.edu>
2019-08-02 16:12:21 -0700

    Add set methods for Place.java (#60)

--- src/main/java/com/google/codeu/data/Place.java
+++ src/main/java/com/google/codeu/data/Place.java
@@ -140,17 +140,33 @@ public class Place {
   public String getTitle() {
     return title;
   }
+  
+  public void setTitle(String title) {
+    this.title = title;
+  }
 
   public String getDescription() {
     return description;
   }
 
+  public void setDescription(String description) {
+    this.description = description;
+  }
+  
   public double getLatitude() {
     return latitude;
+  }
+  
+  public void setLatitude(double latitude) {
+    this.latitude = latitude;
   }
 
   public double getLongitude() {
     return longitude;
+  }
+
+  public void setLongitude(double longitude) {
+    this.longitude = longitude;
   }
 
   public long getTimestamp() {

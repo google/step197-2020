b2d564e9e0bf3662c87cd04126ae7c74ab3fb78f
Thomas <thomasngo05@gmail.com>
2020-07-30 17:32:35 +0000

    Update Card, Folder, User classes

--- src/main/java/com/google/sps/data/Card.java
+++ src/main/java/com/google/sps/data/Card.java
@@ -1,6 +1,12 @@
 package com.google.sps.data;
 
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+
 public final class Card {
+
     private String id;
     private String blobKey;
     private String labels;
@@ -8,9 +14,9 @@ public final class Card {
     private String toLang;
     private String textNotTranslated;
     private String textTranslated;
+    private String cardKey;
       
     public Card (
-        String id,
         String blobKey,
         String labels,
         String fromLang,
@@ -18,17 +24,13 @@ public final class Card {
         String textNotTranslated,
         String textTranslated) {
             
-      this.id = id;
       this.blobKey = blobKey;
       this.labels = labels;
       this.fromLang = fromLang;
       this.toLang = toLang;
       this.textNotTranslated = textNotTranslated;
       this.textTranslated = textTranslated;
-    }
-
-    public String getId() {
-        return this.id;
+      this.cardKey = "null";
     }
 
     public String getBlobKey() {
@@ -55,6 +57,10 @@ public final class Card {
         return this.textTranslated;
     }
 
+    public String getCardKey() {
+        return this.cardKey;
+    }
+
     public void setBlobKey(String newBlobKey) {
         this.blobKey = blobKey;
     }
@@ -78,5 +84,39 @@ public final class Card {
 
     public void setTextTranslated(String newText) {
         this.textTranslated = newText;
+    }
+
+    public void setCardKey(String cardKey) {
+        this.cardKey = cardKey;
+    }
+
+    public Entity createEntity(Key folderKey) {
+        
+        Entity card = new Entity("Card", folderKey);
+        card.setProperty("blobKey", this.blobKey);
+        card.setProperty("labels", this.labels);
+        card.setProperty("fromLang", this.fromLang);
+        card.setProperty("toLang", this.toLang);
+        card.setProperty("textNotTranslated", this.textNotTranslated);
+        card.setProperty("textTranslated", this.textTranslated);
+        card.setProperty("cardKey", KeyFactory.keyToString(card.getKey()));
+
+        return card;
+    }
+
+    public static Card EntityToCard(Entity entity) throws EntityNotFoundException {
+
+        String blobKey = (String) entity.getProperty("blobKey");
+        String labels = (String) entity.getProperty("labels");
+        String fromLang = (String) entity.getProperty("fromLang");
+        String toLang = (String) entity.getProperty("toLang");
+        String textNotTranslated = (String) entity.getProperty("textNotTranslated");
+        String textTranslated = (String) entity.getProperty("textTranslated");
+        String cardKey = (String) entity.getProperty("cardKey");
+
+        Card card = new Card(blobKey, labels, fromLang, toLang, textNotTranslated, textTranslated);
+        card.setCardKey(cardKey);
+        
+        return card;
     }
 }
--- src/main/java/com/google/sps/data/Folder.java
+++ src/main/java/com/google/sps/data/Folder.java
@@ -1,23 +1,27 @@
 package com.google.sps.data;
 
+import com.google.appengine.api.datastore.Entity;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.datastore.EntityNotFoundException;
+
 public final class Folder {
 
-    private String id;
     private String folderName;
     private String folderDefaultLanguage;
+    private String folderKey;
 
     public Folder(
-        String id,
         String folderName,
         String folderDefaultLanguage) {
         
-        this.id = id;
         this.folderName = folderName;
         this.folderDefaultLanguage = folderDefaultLanguage;
+        this.folderKey = "null";
     }
 
     public String getId() {
-        return this.id;
+        return this.userId;
     }
 
     public String getFolderName() {
@@ -28,8 +32,40 @@ public final class Folder {
         return this.folderDefaultLanguage;
     }
 
+    public String getFolderKey() {
+        return this.folderKey;
+    }
+
     public void setFolderName(String newFolderName) {
         this.folderName = newFolderName;
     }
-    
+
+    public void setFolderKey(String folderKey) {
+        this.folderKey = folderKey;
+    }
+
+    public Entity createEntity(Key userKey) {
+        
+        // Set owner of folder 
+        Entity folder = new Entity("Folder", userKey);
+
+        folder.setProperty("folderName", this.folderName);
+        folder.setProperty("folderDefaultLanguage", this.folderDefaultLanguage);
+        folder.setProperty("folderKey", KeyFactory.keyToString(folder.getKey()));
+
+        return folder;
+    }
+
+    // Returns a folder instance from a given Entity
+    public static Folder EntityToFolder(Entity entity) throws EntityNotFoundException {
+        
+        String folderName = (String) entity.getProperty("folderName");
+        String folderDefaultLanguage = (String) entity.getProperty("folderDefaultLanguage");
+        String folderKey = (String) entity.getProperty("folderKey");
+
+        Folder folder = new Folder(userId, folderName, folderDefaultLanguage);
+        folder.setFolderKey(folderKey);
+
+        return folder;
+    }
 }

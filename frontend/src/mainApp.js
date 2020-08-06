264bfa37201ba88db93ceb0c8c1573053cb5accb
Evan Saracay <evansaracay@gmail.com>
2020-08-06 13:30:29 -0700

    Add library for parsing search query's

--- frontend/src/mainApp.js
+++ frontend/src/mainApp.js
@@ -9,17 +9,16 @@ import ImageInterface from "./pages/ImageInterface";
 import PageError from "./pages/404";
 import css from "./mainApp.css";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { match } from "react-router-dom"
 
 // This file handles all routing for our app pages
 function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/CreateCard">
-          <CreateCard></CreateCard>
-        </Route>
-        <Route exact path="/MyFolders" component={MyFolders} />
-        <Route exact path="/ImageInterface" component={ImageInterface} />
+        <Route exact path="/CreateCard" component={CreateCard}></Route>
+        <Route  exact path="/myFolders" component={MyFolders} />
+        <Route  exact path="/ImageInterface" component={ImageInterface} />
         <Route exact path="/YoutubeInterface" component={YoutubeDetect} />
         <Route exact path="/CreateFolder" component={CreateFolder} />
         <Route exact path="/InsideFolder" component={InsideFolder} />

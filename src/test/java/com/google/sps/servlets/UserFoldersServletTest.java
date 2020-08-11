ddde8c706aeddb61ddf34f77649ed6e18a5b2556
Thomas <thomasngo05@gmail.com>
2020-08-11 20:07:59 +0000

    Hardcode JSON response for unit tests in UserFoldersServletTest

--- src/test/java/com/google/sps/servlets/UserFoldersServletTest.java
+++ src/test/java/com/google/sps/servlets/UserFoldersServletTest.java
@@ -83,7 +83,7 @@ public final class UserFoldersServletTest {
   }
   
   @Test
-  public void QueryUserFolders() throws Exception {
+  public void queryUserFolders() throws Exception {
     /* Returns array of Folders and signals front-end to show Create Folder Option */
 
     // Generate testing folders to store in datastore
@@ -99,13 +99,17 @@ public final class UserFoldersServletTest {
     List<Folder> foldersInDatastore = EntityTestingTool.populateDatastoreWithFolders(folderA, folderB, datastore, userKey);
     servlet.doGet(mockRequest, mockResponse);
     String response = responseWriter.toString();
-    String expectedResponse = new Gson().toJson(getExpectedJsonInfo(/*folder=*/foldersInDatastore, /*showCreateFormStatus=*/true));
+    String expectedResponse = 
+      "{\"userFolders\":["
+        +  "{\"folderName\":\"FIRSTFOLDER\",\"folderDefaultLanguage\":\"en\",\"folderKey\":\""+ folderAInDatastore.getFolderKey() +"\"},"
+        +  "{\"folderName\":\"SECONDFOLDER\",\"folderDefaultLanguage\":\"en\",\"folderKey\":\""+ folderBInDatastore.getFolderKey() +"\"}],"
+        + "\"showCreateFormStatus\":true}";
 
     assertTrue(compareJson(response, expectedResponse));
   }
 
   @Test
-  public void UserHasNoCurrentFolder() throws Exception {
+  public void userHasNoCurrentFolder() throws Exception {
     /* Returns an empty array list and signals front-end to show Create Folder Option */
 
     List<Folder> noFoldersInDatastore = new ArrayList<>();
@@ -117,13 +121,13 @@ public final class UserFoldersServletTest {
     when(mockRequest.getParameter("userKey")).thenReturn(userKey);
     servlet.doGet(mockRequest, mockResponse);
     String response = responseWriter.toString();
-    String expectedResponse = new Gson().toJson(getExpectedJsonInfo(/*folder=*/noFoldersInDatastore, /*showCreateFormStatus=*/true));
+    String expectedResponse = "{\"userFolders\":[],\"showCreateFormStatus\":true}";
 
     assertTrue(compareJson(response, expectedResponse));
   }
 
   @Test
-  public void UserNotLoggedIn() throws Exception {
+  public void userNotLoggedIn() throws Exception {
     /* Returns an empty array list and signals front-end to not show Create Folder Option */
 
     helper.setEnvIsLoggedIn(false);
@@ -131,13 +135,13 @@ public final class UserFoldersServletTest {
 
     servlet.doGet(mockRequest, mockResponse);
     String response = responseWriter.toString();
-    String expectedResponse = new Gson().toJson(getExpectedJsonInfo(/*folder=*/noFoldersQueried, /*showCreateFormStatus=*/false));
+    String expectedResponse = "{\"userFolders\":[],\"showCreateFormStatus\":false}";
 
     assertTrue(compareJson(response, expectedResponse));
   }
 
   @Test
-  public void UserCreatesFirstFolder() throws Exception {
+  public void userCreatesFirstFolder() throws Exception {
     /* First time user is creating a folder, so current number of folders should be 1 */
     
     // Generate testing User
@@ -153,7 +157,7 @@ public final class UserFoldersServletTest {
   }
 
   @Test
-  public void UserCreatesAFolderAndHasMultipleFoldersAlready() throws Exception {
+  public void userCreatesAFolderAndHasMultipleFoldersAlready() throws Exception {
     /* Return the current number of folders that user has after creating a folder */
 
     // Generate testing folders to store in datastore
@@ -172,15 +176,5 @@ public final class UserFoldersServletTest {
 
     servlet.doPost(mockRequest, mockResponse);
     assertEquals(3, datastore.prepare(new Query("Folder").setAncestor(user.getKey())).countEntities(withLimit(10)));
-
-  }
-
-  public Map<String, Object> getExpectedJsonInfo(List<Folder> folders, boolean showCreateFormStatus) {
-
-    Map<String, Object> expectedJsonInfo = new HashMap<>();
-    expectedJsonInfo.put("showCreateFormStatus", showCreateFormStatus);
-    expectedJsonInfo.put("userFolders", folders);
-
-    return expectedJsonInfo;
   }
 }

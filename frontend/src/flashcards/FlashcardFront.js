3ffc00b99e2d56e66d5cb39d927b9f87816e16b1
Jennifer <jppham@google.com>
2020-08-14 16:38:34 -0700

    Convert class component to function component

--- frontend/src/flashcards/FlashcardFront.js
+++ frontend/src/flashcards/FlashcardFront.js
@@ -1,54 +1,38 @@
-import React from "react";
+import React from 'react';
 
-/* Styling for the front of the flashcard*/
 const frontStyle = {
-  borderRadius: "30px",
-  position: "absolute",
-  width: "300px",
-  height: "350px",
-  backgroundColor: "white",
-  color: "black",
-  boxShadow: "rgba(0, 0, 0, 0.2) 0px 1px 8px",
-  fontFamily: "'Montserrat', sans-serif",
-  fontSize: "30px",
-  fontWeight: "600",
-  textAlign: "center",
-  display: "flex",
-  flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
+	borderRadius: '30px',
+	position: 'absolute',
+	width: '300px',
+	height: '350px',
+	backgroundColor: 'white',
+	color: 'black',
+	boxShadow: 'rgba(0, 0, 0, 0.2) 0px 1px 8px',
+	fontFamily: "'Montserrat', sans-serif",
+	fontSize: '30px',
+	fontWeight: '600',
+	textAlign: 'center',
+	display: 'flex',
+	flexDirection: 'column',
+	justifyContent: 'center',
+	alignItems: 'center',
 };
-/*
-  Styling for the image inside the flashcard
-*/
+
 const imgStyle = {
-  display: "block",
-  maxWidth: "150px",
-  maxHeight: "150px",
-  width: "auto",
-  height: "auto",
-  margin: "10px",
+	display: 'block',
+	maxWidth: '150px',
+	maxHeight: '150px',
+	width: 'auto',
+	height: 'auto',
+	margin: '10px',
 };
 
-class FlashcardFront extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="FlashcardFront" style={frontStyle}>
-        {this.props.image ? (
-          <img
-            style={imgStyle}
-            src={this.props.image}
-            alt="Can not fetch image"
-          />
-        ) : null}
-        {this.props.text}
-      </div>
-    );
-  }
+function FlashcardFront(props) {
+	return (
+		<div className="FlashcardFront" style={frontStyle}>
+			{this.props.image ? <img style={imgStyle} src={this.props.image} alt="Can not fetch image" /> : null}
+			{this.props.text}
+		</div>
+	);
 }
 export default FlashcardFront;

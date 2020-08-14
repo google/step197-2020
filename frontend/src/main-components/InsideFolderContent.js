4879cc427be8767035f30719d4212d08265dcb5f
Jennifer <jppham@google.com>
2020-08-14 16:18:03 -0700

    Fix try-catch in promise

--- frontend/src/main-components/InsideFolderContent.js
+++ frontend/src/main-components/InsideFolderContent.js
@@ -1,67 +1,57 @@
-import React from "react";
-import styled from "@emotion/styled";
-import Flashcard from "../flashcards/Flashcard.js";
-import NewCard from "../flashcards/NewCard.js";
-
-const DebugMessage = (message, isOn) => {
-  if (isOn) {
-    console.log(message);
-  }
-};
-
-const debuggerMessageOn = false;
+import React from 'react';
+import styled from '@emotion/styled';
+import Flashcard from '../flashcards/Flashcard.js';
+import NewCard from '../flashcards/NewCard.js';
+import { LogDebugMessage } from './LogDebugMessage.js';
 
 const InsideFolderContent = (props) => {
-  const Container = styled.div`
-    flex: 9 1 auto;
-    display: flex;
-    border: 1rem solid white;
-    border-radius: 1rem;
-    background-color: white;
-    margin: 1%;
-    flex-direction: column;
-  `;
+	const Container = styled.div`
+		flex: 9 1 auto;
+		display: flex;
+		border: 1rem solid white;
+		border-radius: 1rem;
+		background-color: white;
+		margin: 1%;
+		flex-direction: column;
+	`;
 
-  let flashcards;
-  try {
-    fetch(`/usercards?folderKey=${folderKey}`, { method: "GET" })
-      .then((result) => result.json())
-      .then((data) => {
-        flashcards = data.map((flashcard) => (
-          <Flashcard
-            key={flashcard.cardKey}
-            image={flashcard.blobKey}
-            text={flashcard.rawText}
-            translation={flashcard.textTranslated}
-            labels={flashcard.labels}
-          />
-        ));
-      });
-  } catch (err) {
-    DebugMessage("Can not fetch flashcards", debuggerMessageOn);
-  }
+	let flashcards = [];
+	const FetchPromise = fetch(`/usercards?folderKey=${folderKey}`, { method: 'GET' });
+	FetchPromise.then((result) => result.json())
+		.then((data) => {
+			flashcards = data.map((flashcard) => (
+				<Flashcard
+					key={flashcard.cardKey}
+					image={flashcard.blobKey}
+					text={flashcard.rawText}
+					translation={flashcard.textTranslated}
+					labels={flashcard.labels}
+				/>
+			));
+		})
+		.catch((error) => {
+			LogDebugMessage('Can not fetch flashcards');
+		});
 
-  const CardContainer = {
-    display: "flex",
-    flexFlow: "row wrap",
-    justifyContent: "space-around",
-    alignItems: "start",
-  };
-  let count = 0;
+	const CardContainer = {
+		display: 'flex',
+		flexFlow: 'row wrap',
+		justifyContent: 'space-around',
+		alignItems: 'start',
+	};
 
-  if (flashcards) {
-    count = flashcards.length;
-  }
-  flashcards.unshift(<NewCard />);
-  return (
-    <div style={{ flex: "9", display: "flex" }}>
-      <Container>
-        <h1>Your Folder has {count} cards</h1>
-        <br></br>
-        <div style={CardContainer}>{flashcards}</div>
-      </Container>
-    </div>
-  );
+	const count = flashcards?.length;
+
+	flashcards.unshift(<NewCard />);
+	return (
+		<div style={{ flex: '9', display: 'flex' }}>
+			<Container>
+				<h1>Your Folder has {count} cards</h1>
+				<br></br>
+				<div style={CardContainer}>{flashcards}</div>
+			</Container>
+		</div>
+	);
 };
 
 export default InsideFolderContent;

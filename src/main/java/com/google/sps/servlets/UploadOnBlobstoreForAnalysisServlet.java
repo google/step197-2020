3c7df1d4d09ac6e887d18fb3eb9c62b64c662a83
Jennifer <jppham@google.com>
2020-08-25 08:24:34 -0700

    Revised spacing

--- src/main/java/com/google/sps/servlets/UploadOnBlobstoreForAnalysisServlet.java
+++ src/main/java/com/google/sps/servlets/UploadOnBlobstoreForAnalysisServlet.java
@@ -7,15 +7,11 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/uploadForImageAnalysis")
 public class UploadOnBlobstoreServlet extends HttpServlet {
 
-  // Create an upload URL for the user to upload their images to BlobStore
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
 
-    // Redirects the user to /usercards servlet upon completion of image being processed in
-    // Blobstore
     String uploadUrl = blobstoreService.createUploadUrl("/imageAnalysis");
-
     response.setContentType("text/html");
     response.getWriter().println(uploadUrl);
   }

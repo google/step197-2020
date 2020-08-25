349ee0fe909720b01a90ff826863334d6766e626
ngothomas <thomasngo05@gmail.com>
2020-08-24 17:01:42 -0700

    Implement RecommendationServlets and its tests

---
+++ src/main/java/com/google/sps/servlets/RecommendationServlet.java
@@ -0,0 +1,67 @@
+package com.google.sps.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.sps.tool.ResponseSerializer;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.file.Paths;
+
+import org.mapdb.*;
+import com.google.gson.Gson;
+import java.util.Map;
+import java.util.HashMap;
+
+@WebServlet("/recommendation")
+public class RecommendationServlet extends HttpServlet {
+  private DB mainDB;
+  private BTreeMap<String, String[]> queryNearestNeighbors;
+
+  @Override
+  public void init() {
+    String path = Paths.get("/home/ngothomas/downloads/webapp/step197-2020/word2vec.db").toString();
+    mainDB = DBMaker.fileDB(path).make();
+    queryNearestNeighbors =
+        mainDB
+            .treeMap("Main")
+            .keySerializer(Serializer.STRING)
+            .valueSerializer(Serializer.JAVA)
+            .createOrOpen();
+  }
+
+  @Override
+  public void destroy() {
+    mainDB.close();
+  }
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      String jsonErrorInfo = ResponseSerializer.getErrorJson("User not logged in");
+      response.setContentType("application/json;");
+      response.getWriter().println(new Gson().toJson(jsonErrorInfo));
+      return;
+    }
+
+    String queryWord = request.getParameter("queryWord");
+    int numOfWordsRequested = Integer.parseInt(request.getParameter("numOfWordsRequested"));
+
+    String[] neighbors = queryNearestNeighbors.get(queryWord);
+
+    // We skip neighbors[0] because that is the same word as "queryWord"
+    String[] requestedNeighbors = new String[numOfWordsRequested];
+    for (int i = 1; i < numOfWordsRequested + 1; i++) {
+      requestedNeighbors[i - 1] = neighbors[i];
+    }
+
+    Map<String, String[]> jsonInfo = new HashMap<>();
+    jsonInfo.put(queryWord, requestedNeighbors);
+    response.setContentType("application/json;");
+    response.getWriter().println(new Gson().toJson(jsonInfo));
+  }
+}
---
+++ src/test/java/com/google/sps/servlets/RecommendationServletTest.java
@@ -0,0 +1,83 @@
+package com.google.sps.servlets;
+
+import static com.google.sps.tool.Tool.compareJson;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.After;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.tools.development.testing.LocalUserServiceTestConfig;
+import com.google.appengine.tools.development.testing.LocalServiceTestHelper;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class RecommendationServletTest {
+  private final LocalServiceTestHelper helper =
+      new LocalServiceTestHelper(new LocalUserServiceTestConfig())
+          .setEnvIsAdmin(true)
+          .setEnvIsLoggedIn(true)
+          .setEnvEmail("test@gmail.com")
+          .setEnvAuthDomain("gmail.com");
+
+  private HttpServletRequest mockRequest;
+  private HttpServletResponse mockResponse;
+  private StringWriter responseWriter;
+  private RecommendationServlet servlet;
+
+  @Before
+  public void setUp() throws Exception {
+    helper.setUp();
+    servlet = new RecommendationServlet();
+    servlet.init();
+    mockRequest = mock(HttpServletRequest.class);
+    mockResponse = mock(HttpServletResponse.class);
+
+    // Set up a fake HTTP response
+    responseWriter = new StringWriter();
+    when(mockResponse.getWriter()).thenReturn(new PrintWriter(responseWriter));
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    helper.setEnvIsLoggedIn(true);
+    servlet.destroy();
+    helper.tearDown();
+  }
+
+  @Test
+  public void queryForCandyTop10NearestNeighbors() throws Exception {
+    when(mockRequest.getParameter("queryWord")).thenReturn("candy");
+    when(mockRequest.getParameter("numOfWordsRequested")).thenReturn("10");
+
+    servlet.doGet(mockRequest, mockResponse);
+    String response = responseWriter.toString();
+    String expectedResponse =
+        "{\"candy\":"
+            + "[\"candies\",\"sweets\",\"chocolate\",\"chocolates\",\"lollipops\","
+            + "\"lollypops\",\"gobstoppers\",\"lollies\",\"snacks\",\"soda\"]}";
+    assertTrue(compareJson(response, expectedResponse));
+  }
+
+  @Test
+  public void queryForElephantTop15NearestNeighbors() throws Exception {
+    when(mockRequest.getParameter("queryWord")).thenReturn("elephant");
+    when(mockRequest.getParameter("numOfWordsRequested")).thenReturn("15");
+
+    servlet.doGet(mockRequest, mockResponse);
+    String response = responseWriter.toString();
+    String expectedResponse =
+        "{\"elephant\":"
+            + "[\"elephants\",\"rhino\",\"pachyderm\",\"tiger\",\"rhinoceros\","
+            + "\"tigers\",\"hippo\",\"gorilla\",\"giraffe\",\"rhinos\","
+            + "\"hippopotamus\",\"pachyderms\",\"rhinoceroses\",\"orangutan\",\"lions\"]}";
+    assertTrue(compareJson(response, expectedResponse));
+  }
+}

e478fb6edde49111554df7672bf30fa2e6084037
Jennifer <32044772+Froggerson@users.noreply.github.com>
2020-08-28 15:33:43 -0700

    Implement Vision servlet frontend (#35)
    * Add basic frontend for Image anaylsis
    
    * Add Cropping feature
    
    * Add Label Scroll
    
    * Fix formatting and add post function
    
    * Fixed errors with comments
    
    * Fixed Spacing

--- frontend/src/main-components/ImageInterfaceContent.js
+++ frontend/src/main-components/ImageInterfaceContent.js
@@ -1,22 +1,213 @@
 import React from "react";
 import styled from "@emotion/styled";
+import "./ImageInterfaceContent.css";
+import Cropper from "react-easy-crop";
+import getCroppedImg from "../sub-components/CropImage";
+import LabelScroll from "../sub-components/labelScroll";
+import { Link } from "react-router-dom";
 
-const ImageInterfaceContent = (props) => {
-  const Container = styled.div`
-    flex: 9;
-    display: flex;
-    border: 1rem solid white;
-    border-radius: 1rem;
-    background-color: white;
-    margin: 1%;
-  `;
+const CONTAINER_HEIGHT = 500;
+class ImageInterfaceContent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      imageUploaded: false,
+      imageCropped: false,
+      imageURL: "",
+      crop: { x: 0, y: 0 },
+      zoom: 1,
+      aspect: 4 / 3,
+      rotation: 0,
+      croppedAreaPixels: {
+        x: 0,
+        y: 0,
+        width: 0,
+        height: 0,
+      },
+      croppedImageURL: "",
+      labels: [],
+      selectedLabel: "Please select a label",
+      imageUploadUrl: "",
+      uploadUrlFetched: false,
+    };
+    this.imageSelected = this.imageSelected.bind(this);
+    this.handleImageUpload = this.handleImageUpload.bind(this);
+    this.handleSelectedLabel = this.handleSelectedLabel.bind(this);
+    this.handleImageCrop = this.handleImageCrop.bind(this);
+    this.onCropChange = this.onCropChange.bind(this);
+    this.handleZoom = this.handleZoom.bind(this);
+    this.handleReset = this.handleReset.bind(this);
+    this.setCroppedAreaPixels = this.setCroppedAreaPixels.bind(this);
+    this.fetchLabels = this.fetchLabels.bind(this);
+  }
 
-  //Code for new page goes here
-  return (
-    <Container>
-      <h1>Detecting Objects in Image</h1>
-    </Container>
-  );
-};
+  imageSelected(event) {
+    this.setState({ imageURL: URL.createObjectURL(event.target.files[0]) });
+  }
+  handleSelectedLabel(domEvent) {
+    const selectedValue = domEvent.target[domEvent.target.selectedIndex].value;
+    this.setState({ selectedLabel: selectedValue });
+  }
+
+  handleImageUpload(event) {
+    this.setState({ imageUploaded: true });
+  }
+
+  handleImageCrop(event) {
+    const getImage = async () => {
+      const croppedImage = await getCroppedImg(
+        this.state.imageURL,
+        this.state.croppedAreaPixels,
+        this.state.rotation
+      );
+      this.setState({ imageCropped: true, croppedImageURL: croppedImage });
+      this.fetchLabels();
+    };
+    getImage();
+  }
+
+  onCropChange(crop) {
+    this.setState({ crop });
+  }
+
+  handleZoom(zoom) {
+    this.setState({ zoom });
+  }
+
+  handleReset() {
+    this.setState({ zoom: 1, crop: { x: 0, y: 0 } });
+  }
+
+  setCroppedAreaPixels(croppedArea, croppedAreaPixels) {
+    this.setState({ croppedAreaPixels: croppedAreaPixels });
+  }
+
+  async componentDidMount() {
+    try {
+      const uploadUrl = await fetch("/upload").then((response) =>
+        response.text()
+      );
+      if (!uploadUrl.ok) {
+        throw Error(uploadUrl.statusText);
+      }
+      this.setState({ imageUploadUrl: uploadUrl, uploadUrlFetched: true });
+    } catch (error) {
+      alert("Please refresh page to upload image");
+    }
+  }
+
+  async postData(url = "", image = "") {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(image),
+    });
+    return response.text();
+  }
+
+  fetchLabels() {
+    postData("/ObjectDectectionUpload", this.state.croppedImageURL)
+      .then((labels) => {
+        this.setState({ labels: labels });
+      })
+      .catch((error) => {
+        alert("Could not fetch labels, please try refreshing the page.");
+      });
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div className="contentContainer">
+          {this.state.imageCropped ? (
+            <div className="formContainer">
+              <p>Please select a label that best describes the image:</p>
+              <form>
+                <LabelScroll
+                  labels={this.state.labels}
+                  clickFunc={this.handleSelectedLabel}
+                  selected={this.state.selectedLabel}
+                  name="selectedLabel"
+                ></LabelScroll>
+              </form>
+              <Link
+                to={`/CreateCard?image=${this.state.croppedImageURL}?text=${this.state.selectedLabel}`}
+              >
+                <button className="button">
+                  Create flashcard with this label
+                </button>
+              </Link>
+            </div>
+          ) : (
+            <div className="formContainer">
+              <form id="imageForm">
+                <label for="img">Please Upload an Image for Analysis:</label>
+                <br />
+                <input
+                  onChange={this.imageSelected}
+                  type="file"
+                  id="img"
+                  name="img"
+                  accept="image/*"
+                />
+              </form>
+              <br />
+              {this.state.imageURL ? (
+                <button className="button" onClick={this.handleImageUpload}>
+                  Upload this Image
+                </button>
+              ) : null}
+            </div>
+          )}
+          {this.state.imageUploaded ? (
+            this.state.imageCropped ? (
+              <div className="imageContainer">
+                <img
+                  className="croppedImage"
+                  src={this.state.croppedImageURL}
+                />
+              </div>
+            ) : (
+              <div className="imageContainer">
+                <div className="cropContainer">
+                  <Cropper
+                    image={this.state.imageURL}
+                    crop={this.state.crop}
+                    zoom={this.state.zoom}
+                    aspect={this.state.aspect}
+                    onCropChange={this.onCropChange}
+                    onCropComplete={this.setCroppedAreaPixels}
+                    onZoomChange={this.handleZoom}
+                    onMediaLoaded={(mediaSize) => {
+                      onZoomChange(CONTAINER_HEIGHT / mediaSize.naturalHeight);
+                    }}
+                  />
+                </div>
+                <br />
+                <div className="controlsContainer">
+                  <p>Controls: Zoom = Scoll, Pan = MouseDrag</p>
+                  <div className="buttonContainer">
+                    <button className="button" onClick={this.handleReset}>
+                      Reset to Default
+                    </button>
+                    <button className="button" onClick={this.handleImageCrop}>
+                      Crop
+                    </button>
+                  </div>
+                </div>
+              </div>
+            )
+          ) : (
+            <div className="noImageContainer">
+              Please Upload an Image to use the Image Analysis feature.
+            </div>
+          )}
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ImageInterfaceContent;
---
+++ frontend/src/sub-components/CropImage.js
@@ -0,0 +1,55 @@
+const createImage = (url) =>
+  new Promise((resolve, reject) => {
+    const image = new Image();
+    image.addEventListener("load", () => resolve(image));
+    image.addEventListener("error", (error) => reject(error));
+    image.setAttribute("crossOrigin", "anonymous");
+    image.src = url;
+  });
+
+function getRadianAngle(degreeValue) {
+  return (degreeValue * Math.PI) / 180;
+}
+
+/**
+ * @param {File} image - Image File url
+ * @param {Object} pixelCrop - pixelCrop Object provided by react-easy-crop
+ * @param {number} rotation - optional rotation parameter
+ */
+export default async function getCroppedImg(imageSrc, pixelCrop, rotation = 0) {
+  const image = await createImage(imageSrc);
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  const maxSize = Math.max(image.width, image.height);
+  const safeArea = 2 * ((maxSize / 2) * Math.sqrt(2));
+
+  canvas.width = safeArea;
+  canvas.height = safeArea;
+
+  ctx.translate(safeArea / 2, safeArea / 2);
+  ctx.rotate(getRadianAngle(rotation));
+  ctx.translate(-safeArea / 2, -safeArea / 2);
+
+  ctx.drawImage(
+    image,
+    safeArea / 2 - image.width * 0.5,
+    safeArea / 2 - image.height * 0.5
+  );
+  const data = ctx.getImageData(0, 0, safeArea, safeArea);
+
+  canvas.width = pixelCrop.width;
+  canvas.height = pixelCrop.height;
+
+  ctx.putImageData(
+    data,
+    Math.round(0 - safeArea / 2 + image.width * 0.5 - pixelCrop.x),
+    Math.round(0 - safeArea / 2 + image.height * 0.5 - pixelCrop.y)
+  );
+
+  return new Promise((resolve) => {
+    canvas.toBlob((file) => {
+      resolve(URL.createObjectURL(file));
+    }, "image/jpeg");
+  });
+}

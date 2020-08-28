daad50aca2435fc0698ff18ee05adf8062d85245
Jennifer <32044772+Froggerson@users.noreply.github.com>
2020-08-28 10:58:04 -0700

    Implement backend for image analysis page. (#32)
    * Add basic backend for image analysis
    
    * Revised spacing
    
    * Fixed formatting from PRs

---
+++ src/main/java/com/google/sps/servlets/UploadOnBlobstoreForAnalysisServlet.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ObjectDetectionUpoload")
+public class UploadOnBlobstoreServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+
+    String uploadUrl = blobstoreService.createUploadUrl("/ObjectDetection");
+    response.setContentType("text/html");
+    response.getWriter().println(uploadUrl);
+  }
+}
